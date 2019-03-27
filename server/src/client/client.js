2dc38bcd8d959de5066f7f370adef8bdd6250bc8
Grauero <grayero6996@gmail.com>
2019-03-27 16:36:45 +0300

    Implement Redux on client/server

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import Routes from './Routes';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
 const app = (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.hydrate(app, document.querySelector('#root'));
--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,17 @@
 import express from 'express';
 
 import renderer from './utils/renderer';
+import createStore from './utils/createStore';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => res.send(renderer(req)));
+app.get('*', (req, res) => {
+  const store = createStore();
+
+  res.send(renderer(req, store));
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
